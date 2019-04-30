f1856a96708519c2bd0d2fb91a89307ffc57209b
Oh-kyung-tak <38690587+Oh-kyung-tak@users.noreply.github.com>
2019-04-30 20:19:17 +0900

    Create baekjoon_2163.cpp

---
+++ baekjoon_2163.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+	
+	cin >> a >> b;
+
+	cout << (a * b) - 1 << endl;
+}
