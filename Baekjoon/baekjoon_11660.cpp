39b22fe4ca305bd7d57bd84f02ac9317a0e6b815
Oh-kyung-tak <38690587+Oh-kyung-tak@users.noreply.github.com>
2019-06-16 23:48:26 +0900

    06/16 Algorithm

---
+++ Baekjoon/baekjoon_11660.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+#include <string.h>
+#include <vector>
+#include <queue>
+#define MAX 1001
+
+// 2차원 배열 prefix sum 공식
+using namespace std;
+long long sum[1050][1050];
+int R;
+int N;
+
+int main()
+{
+	scanf("%d %d", &R, &N);
+
+	for (int i = 1; i <= R; i++)
+	{
+		for (int j = 1; j <= R; j++)
+		{
+			long long int num;
+			scanf("%lld", &num);
+			sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + num;
+		}
+	}
+
+	while (N--)
+	{
+		int a, b, c, d;
+		scanf("%d %d %d %d", &a, &b, &c, &d);
+		printf("%lld\n", sum[c][d] - sum[a - 1][d] - sum[c][b - 1] + sum[a - 1][b - 1]);
+	}
+}
