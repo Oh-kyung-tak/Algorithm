116bae8ff5f640a1f38321c0089a5085508fa978
Oh-kyung-tak <38690587+Oh-kyung-tak@users.noreply.github.com>
2019-10-25 16:33:03 +0900

    10/25 Algorithm

---
+++ Programmers/Lv_4/programmers_스티커 모으기(2).cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int dp_first[100001];
+int dp_second[100001];
+
+int solution(vector<int> sticker)
+{
+	if (sticker.size() == 1)
+		return sticker[0];
+
+	dp_first[0] = sticker[0];
+	dp_first[1] = sticker[0];
+
+	for (int i = 2; i < sticker.size() - 1; i++)
+		dp_first[i] = max(dp_first[i - 2] + sticker[i], dp_first[i - 1]);
+
+	dp_second[0] = 0;
+	dp_second[1] = sticker[1];
+
+	for (int i = 2; i < sticker.size(); i++)
+		dp_second[i] = max(dp_second[i - 2] + sticker[i], dp_second[i - 1]);
+
+	return max(dp_first[sticker.size() - 2], dp_second[sticker.size() - 1]);
+}
---
+++ Programmers/Lv_4/programmers_쿠키 구입.cpp
@@ -0,0 +1,49 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int sum[2001];
+
+int solution(vector<int> cookie) {
+	int answer = 0;
+
+	if (cookie.size() == 2)
+	{
+		if (cookie[0] == cookie[1])
+			answer = cookie[0];
+	}
+	else
+	{
+		for (int i = 0; i < cookie.size() - 1; i++)
+		{
+			int f_index = i;
+			int f_sum = cookie[i];
+
+			int b_index = i + 1;
+			int b_sum = cookie[i + 1];
+
+			while (true)
+			{
+				if (f_sum == b_sum)
+					answer = max(answer, f_sum);
+
+				if (f_index > 0 && f_sum <= b_sum)
+				{
+					f_index--;
+					f_sum += cookie[f_index];
+				}
+				else if (b_index < cookie.size() - 1 && f_sum >= b_sum)
+				{
+					b_index++;
+					b_sum += cookie[b_index];
+				}
+				else
+					break;
+			}
+		}
+	}
+
+	return answer;
+}
